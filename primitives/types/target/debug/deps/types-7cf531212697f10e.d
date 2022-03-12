6c99f814ee91c36c353b7d2a2d3d6de42dd2b0e0
jtabalon <joseph@tabalon.io>
2022-03-12 14:27:18 -0800

    complete primitives section

---
+++ primitives/types/target/debug/deps/types-7cf531212697f10e.d
@@ -0,0 +1,5 @@
+/Users/jtabalonjr/rust_by_example/primitives/types/target/debug/deps/types-7cf531212697f10e.rmeta: src/main.rs
+
+/Users/jtabalonjr/rust_by_example/primitives/types/target/debug/deps/types-7cf531212697f10e.d: src/main.rs
+
+src/main.rs:
---
+++ primitives/types/target/debug/deps/types-ae61da403580c1ba.d
@@ -0,0 +1,5 @@
+/Users/jtabalonjr/rust_by_example/primitives/types/target/debug/deps/types-ae61da403580c1ba.rmeta: src/main.rs
+
+/Users/jtabalonjr/rust_by_example/primitives/types/target/debug/deps/types-ae61da403580c1ba.d: src/main.rs
+
+src/main.rs:
